b6b2e743f37c289c6d5c72a8d06e25e34498238d
Rokan Uddin <rokanu55@gmail.com>
2020-12-06 12:38:59 +0600

    ProjectDetails Componenent added

--- src/components/project/ProjectList.js
+++ src/components/project/ProjectList.js
@@ -1,29 +1,14 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectList = () => {
     return(
         <div className='project-list section'>
-            <div className='card z-depth-0 project-summery'>
-                <div className='card-content grey-text text-darken-3'>
-                    <span className='card-title'>Project Title</span>
-                    <p>Posted by Rokan</p>
-                    <p className='grey-text'>05 december,2020</p>
-                </div>
-                <div className='card z-depth-0 project-summery'>
-                <div className='card-content grey-text text-darken-3'>
-                    <span className='card-title'>Project Title</span>
-                    <p>Posted by Rokan</p>
-                    <p className='grey-text'>05 december,2020</p>
-                </div>
-            </div>
-            <div className='card z-depth-0 project-summery'>
-                <div className='card-content grey-text text-darken-3'>
-                    <span className='card-title'>Project Title</span>
-                    <p>Posted by Rokan</p>
-                    <p className='grey-text'>05 december,2020</p>
-                </div>
-            </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+
         </div>
     )
 }
---
+++ src/components/project/ProjectSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProjectSummary = () => {
+    return (
+        <div className='card z-depth-0 project-summery'>
+             <div className='card-content grey-text text-darken-3'>
+                <span className='card-title'>Project Title</span>
+                <p>Posted by Rokan</p>
+                <p className='grey-text'>05 december,2020</p>
+            </div>
+        </div>
+    );
+}
+export default ProjectSummary;
