0f996748c71412424784d18f372b04f3f28eda79
Rokan Uddin <rokanu55@gmail.com>
2020-12-07 00:17:54 +0600

    static data used to redux store

--- src/components/project/ProjectSummary.js
+++ src/components/project/ProjectSummary.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
     return (
         <div className='card z-depth-0 project-summery'>
              <div className='card-content grey-text text-darken-3'>
-                <span className='card-title'>Project Title</span>
+                <span className='card-title'> {project.title} </span>
                 <p>Posted by Rokan</p>
                 <p className='grey-text'>05 december,2020</p>
             </div>
