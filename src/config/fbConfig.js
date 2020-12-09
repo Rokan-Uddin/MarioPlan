307ac0b8c8a1a603bdb83bd1d30d835b614e28d0
Rokan Uddin <rokanu55@gmail.com>
2020-12-09 14:17:14 +0600

    add data to firebase using front-end

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,19 +1,17 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/auth'
+import 'firebase/auth';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyAJz4RTm6ZAmwAXgoy3zoispD93TYjIxMo",
-    authDomain: "project-firebase-44a6f.firebaseapp.com",
-    projectId: "project-firebase-44a6f",
-    storageBucket: "project-firebase-44a6f.appspot.com",
-    messagingSenderId: "575083348378",
-    appId: "1:575083348378:web:7bd835141fcca788457a23",
-    measurementId: "G-QD3DS6RM90"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.firestore().setting({ timestampsInSnapshots:true });
-  firebase.analytics();
+const firebaseConfig = {
+  apiKey: "AIzaSyAJz4RTm6ZAmwAXgoy3zoispD93TYjIxMo",
+  authDomain: "project-firebase-44a6f.firebaseapp.com",
+  projectId: "project-firebase-44a6f",
+  storageBucket: "project-firebase-44a6f.appspot.com",
+  messagingSenderId: "575083348378",
+  appId: "1:575083348378:web:7bd835141fcca788457a23",
+  measurementId: "G-QD3DS6RM90"
+};
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true });
 
-  export default firebase;
+export default firebase
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore,applyMiddleware} from 'redux';
+import {createStore,applyMiddleware,compose} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/fbConfig';
 
-const store = createStore(rootReducer,applyMiddleware(thunk));
+const store = createStore(rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})),
+    reactReduxFirebase(fbConfig), // redux binding for firebase
+    reduxFirestore(fbConfig) // redux bindings for firestore
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,7 +28,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
